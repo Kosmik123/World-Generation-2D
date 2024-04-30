77e13723a3233e1d1bb08bd0bf43e5c5b9ff3fb2
Michal <malpa.w.klapkach@gmail.com>
2024-04-30 15:01:34 +0200

    Move TerrainGenerationSettings class to separate file

---
+++ Assets/World Generation/Scripts/TerrainGenerationSettings.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace WorldGeneration2D
+{
+    [CreateAssetMenu]
+    public class TerrainGenerationSettings : ScriptableObject
+    {
+        [SerializeField]
+        private Vector2 perlinOffset;
+        [SerializeField]
+        private Vector2 perlinScale;
+
+        private void Reset()
+        {
+            perlinScale = Vector2.one;
+        }
+
+        public float GetValue(float x, float y)
+        {
+            float xReal = x / perlinScale.x + perlinOffset.x;
+            float yReal = y / perlinScale.y + perlinOffset.y;
+
+            return Mathf.PerlinNoise(xReal, yReal);
+        }
+    }
+}
