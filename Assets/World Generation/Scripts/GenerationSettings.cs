a1a528a374a6fd3cfae6a51137757d6a864dda59
Michal <malpa.w.klapkach@gmail.com>
2024-05-01 02:32:39 +0200

    Add octaves stacking to noise generator

--- Assets/World Generation/Scripts/GenerationSettings.cs
+++ Assets/World Generation/Scripts/GenerationSettings.cs
@@ -6,21 +6,45 @@ namespace WorldGeneration2D
     public class GenerationSettings : ScriptableObject
     {
         [SerializeField]
-        private Vector2 perlinOffset;
+        private Octave[] octaves;
+
         [SerializeField]
-        private Vector2 perlinScale;
+        private Vector2 noiseScale;
 
         private void Reset()
         {
-            perlinScale = Vector2.one;
+            noiseScale = Vector2.one;
         }
 
         public float GetValue(float x, float y)
         {
-            float xReal = x / perlinScale.x + perlinOffset.x;
-            float yReal = y / perlinScale.y + perlinOffset.y;
+            float noiseValue = 0;
+            Vector2 positionScale = noiseScale;
+            float valueScale = 1;
 
-            return Mathf.PerlinNoise(xReal, yReal);
+            int octavesCount = octaves.Length;
+            for (int i = 0; i < octavesCount; i++)
+            {
+                float xReal = x / positionScale.x + octaves[i].Offset.x;
+                float yReal = y / positionScale.y + octaves[i].Offset.y;
+                noiseValue += valueScale * Mathf.PerlinNoise(xReal, yReal);
+
+                valueScale /= 2;
+                positionScale /= 2;
+            }
+
+            float maxPossibleValue = 2 - 1f / (1 << (octavesCount - 1));
+
+            return noiseValue / maxPossibleValue;
         }
     }
+
+    [System.Serializable]
+    public struct Octave
+    {
+        [SerializeField]
+        private Vector2 offset;
+        public readonly Vector2 Offset => offset;
+    }
+
 }
