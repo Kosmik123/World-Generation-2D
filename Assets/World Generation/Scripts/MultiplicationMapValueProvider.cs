163b03da37f69094adf344ddd41f7776c9845766
Michal <malpa.w.klapkach@gmail.com>
2024-05-01 18:31:53 +0200

    Add MultiplicationMapValueProvider class

---
+++ Assets/World Generation/Scripts/MultiplicationMapValueProvider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace WorldGeneration2D
+{
+    [CreateAssetMenu]
+    public class MultiplicationMapValueProvider : MapValueProvider
+    {
+        [SerializeField]
+        private MapValueProvider[] mapValueProviders;
+
+        public override float GetValue(float x, float y)
+        {
+            float value = 1;
+            foreach(var mapValueProvider in mapValueProviders)
+                value *= mapValueProvider.GetValue(x, y);
+
+            return value;
+        }
+    }
+}
